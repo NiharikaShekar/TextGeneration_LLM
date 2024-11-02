bfe5696541f9e2ecd403d46aab6bb9062b841d8c
NiharikaShekar <niharikashekar17@gmail.com>
2024-11-02 03:40:52 -0500

    code update with embeddings

--- src/main/scala/SlidingWindow.scala
+++ src/main/scala/SlidingWindow.scala
@@ -1,272 +1,260 @@
-import org.apache.spark.{SparkConf, SparkContext}
-import org.apache.spark.rdd.RDD
+import org.apache.spark.sql.SparkSession
+import org.apache.spark.ml.linalg.{Vector, Vectors}
+import org.apache.spark.sql.{DataFrame, Row}
+import org.apache.spark.sql.types._
+import org.apache.spark.ml.linalg.SQLDataTypes.VectorType
 import org.deeplearning4j.nn.conf.NeuralNetConfiguration
 import org.deeplearning4j.nn.conf.layers.{DenseLayer, OutputLayer}
 import org.deeplearning4j.nn.multilayer.MultiLayerNetwork
 import org.deeplearning4j.nn.weights.WeightInit
 import org.deeplearning4j.optimize.listeners.ScoreIterationListener
 import org.nd4j.linalg.activations.Activation
-import org.nd4j.linalg.api.ndarray.INDArray
+import org.nd4j.linalg.dataset.DataSet
 import org.nd4j.linalg.factory.Nd4j
 import org.nd4j.linalg.learning.config.Adam
 import org.nd4j.linalg.lossfunctions.LossFunctions
-import org.nd4j.linalg.ops.transforms.Transforms
-
+import org.nd4j.linalg.api.ndarray.INDArray  // Added this import
+import org.nd4j.linalg.indexing.NDArrayIndex
+import org.deeplearning4j.util.ModelSerializer
 import scala.collection.mutable.ArrayBuffer
 import scala.util.Random
+import java.io.File
+import org.nd4j.linalg.ops.transforms.Transforms
 
-class SentenceGeneration extends Serializable {
-  val vocabularySize: Int = 1000
-  val embeddingSize: Int = 64
-  val windowSize: Int = 1
-  val batchSize: Int = 32
+object SlidingWindow {
+  // Constants
+  val embeddingSize = 64
+  val windowSize = 3
+  val batchSize = 32
 
-  // Tokenizer class for handling text conversion
-  class SimpleTokenizer extends Serializable {
-    private var wordToIndex = Map[String, Int]()
-    private var indexToWord = Map[Int, String]()
-    private var currentIdx = 0
-
-    def fit(texts: Seq[String]): Unit = {
-      texts.flatMap(_.split("\\s+")).distinct.foreach { word =>
-        if (!wordToIndex.contains(word)) {
-          wordToIndex += (word -> currentIdx)
-          indexToWord += (currentIdx -> word)
-          currentIdx += 1
+  def computePositionalEmbedding(windowSize: Int, embeddingDim: Int): Array[Vector] = {
+    val positionalEncodings = new Array[Vector](windowSize)
+    for (pos <- 0 until windowSize) {
+      val embedding = new Array[Double](embeddingDim)
+      for (i <- 0 until embeddingDim by 2) {
+        val angle = pos / math.pow(10000, (2.0 * i) / embeddingDim)
+        embedding(i) = math.sin(angle)
+        if (i + 1 < embeddingDim) {
+          embedding(i + 1) = math.cos(angle)
         }
       }
+      positionalEncodings(pos) = Vectors.dense(embedding)
     }
-
-    def encode(text: String): Seq[Int] = {
-      text.split("\\s+").map(word => wordToIndex.getOrElse(word, 0))
-    }
-
-    def decode(indices: Seq[Int]): String = {
-      indices.map(idx => indexToWord.getOrElse(idx, "")).mkString(" ")
-    }
-  }
-
-  // Create sliding windows for training data
-  def createSlidingWindows(tokens: Seq[Int]): Seq[(Seq[Int], Int)] = {
-    tokens.sliding(windowSize + 1).map { window =>
-      (window.init, window.last)
-    }.toSeq
-  }
-
-  // Convert sequence to embedding matrix with positional encoding
-  def createEmbeddingMatrix(sequence: Seq[Int]): INDArray = {
-    val embedding = Nd4j.zeros(1, embeddingSize, sequence.length)
-
-    // Create word embeddings
-    sequence.zipWithIndex.foreach { case (token, pos) =>
-      val tokenEmbedding = Nd4j.randn(1, embeddingSize).mul(0.1)
-      embedding.putSlice(pos, tokenEmbedding)
-    }
-
-    // Add positional encodings
-    for (pos <- sequence.indices) {
-      for (i <- 0 until embeddingSize) {
-        val angle = pos / math.pow(10000, (2 * i).toFloat / embeddingSize)
-        if (i % 2 == 0) {
-          embedding.putScalar(Array(0, i, pos), embedding.getDouble(0, i, pos) + math.sin(angle))
-        } else {
-          embedding.putScalar(Array(0, i, pos), embedding.getDouble(0, i, pos) + math.cos(angle))
-        }
-      }
-    }
-
-    embedding
+    positionalEncodings
   }
 
   def selfAttention(input: INDArray): INDArray = {
-    val Array(batchSize, sequenceLength, embedSize) = input.shape()
+    // Get dimensions
+    val shape = input.shape()
+    val batchSize = shape(0).toInt
+    val sequenceLength = shape(1).toInt
+    val embedSize = shape(2).toInt
 
-    // Create query, key, and value matrices for each batch independently
-    val query = Nd4j.createUninitialized(batchSize, sequenceLength, embedSize)
-    val key = Nd4j.createUninitialized(batchSize, sequenceLength, embedSize)
-    val value = Nd4j.createUninitialized(batchSize, sequenceLength, embedSize)
+    // Create query, key, and value matrices
+    val query = input.dup()
+    val key = input.dup()
+    val value = input.dup()
 
-    // Compute the dot product between queries and keys
-    val scores = query
-      .tensorAlongDimension(0, 1, 2)
-      .mmul(key.tensorAlongDimension(0, 1, 2).transpose())
+    // Transpose key for matrix multiplication
+    val keyTransposed = key.permute(0, 2, 1)
+
+    // Compute attention scores
+    val scores = Nd4j.matmul(query, keyTransposed)
       .div(math.sqrt(embedSize))
 
-    // Apply softmax along the last dimension to get attention weights
+    // Apply softmax
     val attentionWeights = Transforms.softmax(scores)
 
-    // Multiply the weights with the values
-    val attendedOutput = attentionWeights
-      .tensorAlongDimension(0, 1, 2)
-      .mmul(value.tensorAlongDimension(0, 1, 2))
-
-    attendedOutput.reshape(batchSize, sequenceLength, embedSize)
+    // Compute weighted sum
+    Nd4j.matmul(attentionWeights, value)
   }
 
-  // Improved neural network model
-  def buildModel(): MultiLayerNetwork = {
+  def createModel(inputSize: Int, hiddenSize: Int, outputSize: Int): MultiLayerNetwork = {
     val conf = new NeuralNetConfiguration.Builder()
-      .seed(42)
+      .seed(123)
       .updater(new Adam(0.001))
       .weightInit(WeightInit.XAVIER)
       .list()
       .layer(0, new DenseLayer.Builder()
-        .nIn(embeddingSize * windowSize)
-        .nOut(512)
-        .activation(Activation.RELU)
-        .dropOut(0.2)  // Added dropout for regularization
-        .build())
-      .layer(1, new DenseLayer.Builder()
-        .nIn(512)
-        .nOut(256)
+        .nIn(inputSize)
+        .nOut(hiddenSize)
         .activation(Activation.RELU)
         .dropOut(0.2)
         .build())
-      .layer(2, new OutputLayer.Builder()
-        .nIn(256)
-        .nOut(vocabularySize)
-        .activation(Activation.SOFTMAX)
-        .lossFunction(LossFunctions.LossFunction.NEGATIVELOGLIKELIHOOD)
+      .layer(1, new DenseLayer.Builder()
+        .nIn(hiddenSize)
+        .nOut(hiddenSize / 2)
+        .activation(Activation.RELU)
+        .dropOut(0.2)
+        .build())
+      .layer(2, new OutputLayer.Builder(LossFunctions.LossFunction.MSE)
+        .nIn(hiddenSize / 2)
+        .nOut(outputSize)
+        .activation(Activation.IDENTITY)
         .build())
       .build()
 
     val model = new MultiLayerNetwork(conf)
     model.init()
-    model.setListeners(new ScoreIterationListener(10))  // More frequent updates
+    model.setListeners(new ScoreIterationListener(10))
     model
   }
 
-  // Train the model using Spark RDD
-  def train(sc: SparkContext, textRDD: RDD[String], epochs: Int): MultiLayerNetwork = {
-    val tokenizer = new SimpleTokenizer()
+  def generateText(model: MultiLayerNetwork, df: DataFrame, seedTokens: Array[Row], numWords: Int, temperature: Double = 0.7): Seq[String] = {
+    val embeddingDim = seedTokens.head.getAs[Vector]("embedding").size
+    val positionalEmbeddings = computePositionalEmbedding(windowSize, embeddingDim)
+    val generated = ArrayBuffer[String]()
+    var currentWindow = seedTokens.toBuffer
 
-    // Fit tokenizer and broadcast
-    val allTexts = textRDD.collect()
-    tokenizer.fit(allTexts)
-    val broadcastTokenizer = sc.broadcast(tokenizer)
-
-    // Create and broadcast model
-    val model = buildModel()
-    val broadcastModel = sc.broadcast(model)
-
-    for (epoch <- 1 to epochs) {
-      println(s"Training epoch $epoch")
-
-      // Create training samples using sliding window
-      val samplesRDD = textRDD.flatMap { text =>
-        val tokens = broadcastTokenizer.value.encode(text)
-        createSlidingWindows(tokens)
+    for (_ <- 1 to numWords) {
+      // Prepare input
+      val inputEmbeddings = currentWindow.map(_.getAs[Vector]("embedding"))
+      val positionAwareEmbeddings = inputEmbeddings.zip(positionalEmbeddings).map {
+        case (wordEmb, posEmb) =>
+          val combined = (wordEmb.toArray, posEmb.toArray).zipped.map(_ + _)
+          Vectors.dense(combined)
       }
 
-      // Process batches
-      samplesRDD.foreachPartition { partition =>
-        val localModel = broadcastModel.value
-        val batchBuffer = new ArrayBuffer[(Seq[Int], Int)]()
+      // Convert to Nd4j array and reshape
+      val inputArray = Nd4j.create(positionAwareEmbeddings.flatMap(_.toArray).toArray)
+      val reshapedInput = inputArray.reshape(1, windowSize, embeddingDim)
 
-        partition.foreach { sample =>
-          batchBuffer += sample
-          if (batchBuffer.size >= batchSize) {
-            processBatch(localModel, batchBuffer.toSeq)
-            batchBuffer.clear()
-          }
-        }
+      // Apply self-attention
+      val attentionOutput = selfAttention(reshapedInput)
+      val flattenedInput = attentionOutput.reshape(1, windowSize * embeddingDim)
 
-        // Process remaining samples
-        if (batchBuffer.nonEmpty) {
-          processBatch(localModel, batchBuffer.toSeq)
-        }
-      }
+      // Get prediction
+      val prediction = model.output(flattenedInput)
+
+      // Find nearest neighbor in embedding space
+      val nextToken = findNearestNeighbor(prediction, df)
+      generated += nextToken.getAs[String]("word")
+
+      // Update window
+      currentWindow = currentWindow.tail :+ nextToken
     }
 
-    model
+    generated
   }
 
-  // Process a batch of samples
-  private def processBatch(model: MultiLayerNetwork, batch: Seq[(Seq[Int], Int)]): Unit = {
-    val inputArray = Nd4j.zeros(batch.size, embeddingSize * windowSize)
-    val labelsArray = Nd4j.zeros(batch.size, vocabularySize)
-
-    batch.zipWithIndex.foreach { case ((sequence, label), idx) =>
-      val embedding = createEmbeddingMatrix(sequence)
-      val attentionOutput = selfAttention(embedding)
-      val flattenedAttention = attentionOutput.reshape(1, embeddingSize * windowSize)
-      inputArray.putRow(idx, flattenedAttention)
-      labelsArray.putScalar(Array(idx, label), 1.0)
+  def findNearestNeighbor(prediction: INDArray, df: DataFrame): Row = {
+    // Convert INDArray to Vector
+    val predictionArray = new Array[Double](prediction.length().toInt)
+    for (i <- 0 until prediction.length().toInt) {
+      // Fix: Use getDouble with a long parameter
+      predictionArray(i) = prediction.getDouble(i.toLong)  // Convert i to Long
+      // Alternative fix: Use varargs syntax
+      // predictionArray(i) = prediction.getDouble(i: Int*)
     }
+    val predVector = Vectors.dense(predictionArray)
 
-    model.fit(inputArray, labelsArray)
+    // Find closest embedding
+    val rows = df.collect()
+    rows.minBy { row =>
+      val embedding = row.getAs[Vector]("embedding")
+      Vectors.sqdist(predVector, embedding)
+    }
   }
 
-  /// Modified text generation with temperature sampling
-  def generateText(model: MultiLayerNetwork, tokenizer: SimpleTokenizer, seedText: String, length: Int, temperature: Double = 0.7): String = {
-    var currentSequence = tokenizer.encode(seedText).takeRight(windowSize)
-    val generated = new ArrayBuffer[Int]()
-    val rand = new Random()
-
-    def sampleWithTemperature(logits: INDArray, temp: Double): Int = {
-      val scaled = logits.div(temp)
-      val expScaled = Transforms.exp(scaled)
-      val probs = expScaled.div(expScaled.sum(1))
-
-      // Convert to probabilities and sample
-      val probArray = Array.ofDim[Double](probs.columns())
-      for (i <- 0 until probs.columns()) {
-        probArray(i) = probs.getDouble(Long.box(i))
-      }
-
-      // Sample using cumulative probabilities
-      val cumSum = probArray.scanLeft(0.0)(_ + _).tail
-      val sample = rand.nextDouble()
-      cumSum.zipWithIndex.find(_._1 >= sample).map(_._2).getOrElse(0)
-    }
-
-    for (_ <- 1 to length) {
-      val embedding = createEmbeddingMatrix(currentSequence)
-      val attentionOutput = selfAttention(embedding)
-      val flattenedAttention = attentionOutput.reshape(1, embeddingSize * windowSize)
-
-      val output = model.output(flattenedAttention)
-      val nextTokenIndex = sampleWithTemperature(output, temperature)
-
-      generated += nextTokenIndex
-      currentSequence = (currentSequence.tail :+ nextTokenIndex).takeRight(windowSize)
-    }
-
-    tokenizer.decode(generated)
-  }}
-
-// Usage example
-object SlidingWindow {
   def main(args: Array[String]): Unit = {
-    val conf = new SparkConf().setAppName("SimpleLanguageModel").setMaster("local[*]")
-    val sc = new SparkContext(conf)
+    val spark = SparkSession.builder()
+      .appName("SlidingWindow")
+      .master("local[*]")
+      .getOrCreate()
 
-    // Define the path to the text file
-    val filePath = "/Users/niharikabelavadishekar/Documents/Cloud_Assignment/Homework2_LLM/src/main/resources/input/input.txt" // Replace with the actual file path
+    try {
+      spark.sparkContext.setLogLevel("ERROR")
 
-    // Load the text file into an RDD, where each line in the file is a separate RDD element
-    val textRDD = sc.textFile(filePath)
-      .map(_.trim)                         // Trim whitespace
-      .filter(line => line.nonEmpty)       // Remove empty lines
-      .cache()
+      // Read embeddings data
+      val inputPath = "/Users/niharikabelavadishekar/Documents/Cloud_Assignment/Homework2_LLM/src/main/resources/input/part-r-00000"
+      val data = spark.sparkContext.textFile(inputPath)
 
-    // Create and train the model
-    val model = new SentenceGeneration()
+      // Parse input data with error handling
+      val parsedRows = data.map { line =>
+        try {
+          val parts = line.trim.split("\\s+", 3)
+          if (parts.length == 3) {
+            val word = parts(0)
+            val tokenId = parts(1).toInt
+            val embeddingStr = parts(2).stripPrefix("[").stripSuffix("]")
+            val embedding = Vectors.dense(
+              embeddingStr.split(",").map(_.trim).filter(_.nonEmpty).map(_.toDouble)
+            )
+            Some(Row(word, tokenId, embedding))
+          } else None
+        } catch {
+          case e: Exception =>
+            println(s"Error parsing line: $line - ${e.getMessage}")
+            None
+        }
+      }.filter(_.isDefined).map(_.get)
 
-    // Assuming 'train' method takes an RDD and the number of epochs as arguments
-    val trainedModel = model.train(sc, textRDD, 20)
+      val schema = StructType(Seq(
+        StructField("word", StringType, true),
+        StructField("tokenId", IntegerType, true),
+        StructField("embedding", VectorType, true)
+      ))
 
-    // Correctly initialize and use the tokenizer
-    val tokenizer = new model.SimpleTokenizer()
-    val texts = textRDD.collect() // Collect the RDD to use with the tokenizer
-    tokenizer.fit(texts) // Ensure 'fit' works with collected texts
+      val df = spark.createDataFrame(parsedRows, schema).cache()
 
-    // Generate text using the trained model
-    val generatedText = model.generateText(trainedModel, tokenizer, "Once upon a time, there lived a prince", 50)
-    val cleanedText = generatedText.replaceAll("\\s+", " ")
-    println(s"Generated text: $cleanedText")
+      // Create and process sliding windows
+      val embeddingDim = df.first().getAs[Vector]("embedding").size
+      val positionalEmbeddings = computePositionalEmbedding(windowSize, embeddingDim)
 
-    sc.stop()
+      // Create training datasets
+      val rows = df.collect()
+      val slidingWindows = rows.sliding(windowSize + 1).flatMap { window =>
+        if (window.length == windowSize + 1) {
+          val inputEmbeddings = window.take(windowSize).map(_.getAs[Vector]("embedding"))
+          val positionAwareEmbeddings = inputEmbeddings.zip(positionalEmbeddings).map {
+            case (wordEmb, posEmb) =>
+              val combined = (wordEmb.toArray, posEmb.toArray).zipped.map(_ + _)
+              Vectors.dense(combined)
+          }
+
+          val targetEmbedding = window.last.getAs[Vector]("embedding")
+
+          // Create input array
+          val input = Nd4j.create(positionAwareEmbeddings.flatMap(_.toArray).toArray)
+          val target = Nd4j.create(targetEmbedding.toArray)
+
+          Some(new DataSet(input, target))
+        } else None
+      }.toList
+
+      // Train model
+      val inputSize = windowSize * embeddingDim
+      val hiddenSize = 128
+      val outputSize = embeddingDim
+      val model = createModel(inputSize, hiddenSize, outputSize)
+
+      println("Starting model training...")
+      val numEpochs = 10
+      for (epoch <- 0 until numEpochs) {
+        slidingWindows.grouped(batchSize).foreach { batch =>
+          val features = Nd4j.vstack(batch.map(_.getFeatures): _*)
+          val labels = Nd4j.vstack(batch.map(_.getLabels): _*)
+          model.fit(features, labels)
+        }
+        println(s"Completed epoch $epoch")
+      }
+
+      // Generate sample text
+      val seedTokens = rows.take(windowSize)
+      val generatedText = generateText(model, df, seedTokens, 20)
+      println("Generated text: " + generatedText.mkString(" "))
+
+      // Save model
+      val modelPath = "src/main/resources/output/trained_model.zip"
+      ModelSerializer.writeModel(model, new File(modelPath), true)
+      println(s"Model saved to $modelPath")
+
+    } catch {
+      case e: Exception =>
+        println(s"Application error: ${e.getMessage}")
+        e.printStackTrace()
+    } finally {
+      spark.stop()
+    }
   }
-
 }
